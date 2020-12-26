e4a1b0c4627873ecb814a3f9233f13b1d2b162dd
Moldas Malikhain <kerch999@gmail.com>
2020-12-26 14:13:37 +0200

    romazot

--- src/App.js
+++ src/App.js
@@ -1,27 +1,51 @@
 import './css/App.css';
+import React from 'react'
 import Panel from './js/components/Panel';
-import Header from './js/components/Header'
 import Cart from './js/components/Cart'
 
 
 
-function App() {
-  return (
-    <div className="App">
-      <header><p>Shopping Cart</p></header>
-      <main>
-        <div className = 'content'>
-          <div className='main-content'>
-            <Panel/>
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state ={
+      cartItems: []
+    }
+  }
+
+  addToCart = (product) =>{
+    const cartItems = this.state.cartItems.slice();
+    console.log(product);
+    let alreadyInCart = false;
+    // cartItems.foreEach(item =>{
+    //   if(item.id === product.id)
+    //   item.count++;
+    //   alreadyInCart = true;
+    // });
+    // if(!alreadyInCart){
+    //   cartItems.push({...product, count: 1})
+    // }
+    // this.setState({cartItems});
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header><p>Shopping Cart</p></header>
+        <main>
+          <div className = 'content'>
+            <div className='main-content'>
+              <Panel addToCart={this.addToCart}/>
+            </div>
+            <div className = 'sidebar'>
+              <Cart cartItems={this.state.cartItems}/>
+            </div>
           </div>
-          <div className = 'sidebar'>
-            Cart Items
-          </div>
-        </div>
-      </main>
-      <footer>footer</footer>
-    </div>
-  );
+        </main>
+        <footer>footer</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/js/components/Panel.js
+++ src/js/components/Panel.js
@@ -1,31 +1,33 @@
 import React from "react";
 import '../../css/App.css';
-import {fruits} from '../List';
+import fruits from '../List.json';
 
-const Panel = (props) =>{ 
-    return(
-        <>
-            <ul className = "containers">
-                {fruits.map((data, key) =>{
-                    return( 
-                        <li key = {key+=1}>
-                            <div className="container">
-                                <a href={'#' + data.id}>
-                                    <div className = "image-place"> IMG </div>
-                                    <p className = 'data'>{data.text}</p>
-                                </a>
-                                <hr noshade></hr>
-                                <div className='data-price'>
-                                    <p className = 'data'>{data.price + '$'}</p>
-                                    <button className='panel-button'><span>Add To Cart</span></button>
-                                </div>
-                            </div>
-                        </li>      
-                    );
-                })}
-            </ul>
-        </>
-    );
+class Panel extends React.Component{
     
+    render(){
+        return(
+            <>
+                <ul className = "containers">
+                    {fruits.fruits.map((data, key) =>{
+                        return( 
+                            <li key = {key+=1}>
+                                <div className="container">
+                                    <a href={'#' + data.id}>
+                                        <div className = "image-place"> IMG </div>
+                                        <p className = 'data'>{data.text}</p>
+                                    </a>
+                                    <hr></hr>
+                                    <div className='data-price'>
+                                        <p className = 'data'>{data.price + '$'}</p>
+                                        <button onClick={()=> this.props.addToCart(data)} className='panel-button'><span>Add To Cart</span></button>
+                                    </div>
+                                </div>
+                            </li>      
+                        );
+                    })}
+                </ul>
+            </>
+        );
+    }
 }
 export default Panel;
